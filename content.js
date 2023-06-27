f53df1770d8eff6f1d3ae291208c1e4a48acf16f
Mack S <mackschmaltz@gmail.com>
2023-06-26 23:25:17 -0600

    Temporarily removed lists

--- content.js
+++ content.js
@@ -1,21 +1,18 @@
-var paragraphs = document.querySelectorAll("p, li, h1, h2, h3");
+var paragraphs = document.querySelectorAll("p, h1, h2, h3");
 //<.*>|\s+ for later use
 for (let i = 0; i < paragraphs.length; i++){
     var words = paragraphs[i].innerHTML.split(" ");
     let tempPhrase = "";
-    let tagcheck = false
+    var tagcheck = false
 
     for (let j = 0; j < words.length; j++){     
         
         //ignore tags
-        if (/<*>/.test(words[j])){
-            //do nothing
+        if (/<\/.*>/.test(words[j]) || />/.test(words[j])){
+            tagcheck = false;
         }
         else if (/</.test(words[j])){
             tagcheck = true;
-        }
-        else if (/>/.test(words[j])){
-            tagcheck = false;
         }
 
         else if (!tagcheck){
@@ -23,18 +20,15 @@ for (let i = 0; i < paragraphs.length; i++){
             
             tagremover = words[j].split(">");
             actualString = tagremover[tagremover.length-1].split("");
-            //actualString = words[j].split("");
-            let half = Math.floor(actualString.length/2);
-            //console.log(half);
+            let half = Math.ceil(actualString.length/2);
 
-            for (let k = 0; k<=half; k++){
+            for (let k = 0; k<half; k++){
                 subString += actualString[k];
             }
-            //console.log(subString);
             words[j] = words[j].replace(subString, "<b>" + subString + "</b>");
         
             words[j];
-            //console.log(words[j] + " LOOK HERE");
+
         }
         tempPhrase += words[j] + " ";
         
